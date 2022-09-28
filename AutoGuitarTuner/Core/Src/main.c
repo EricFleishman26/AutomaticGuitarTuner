bca892b654e284c352bdab568ae9a25590fbe958
Eric Fleishman <ericfleishmanwork@gmail.com>
2022-09-27 20:13:14 -0500

    Initial Commit

--- AutoGuitarTuner/Core/Src/main.c
+++ AutoGuitarTuner/Core/Src/main.c
@@ -62,12 +62,6 @@ static void MX_USART2_UART_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-uint32_t IC_Val1 = 0;
-uint32_t IC_Val2 = 0;
-uint32_t difference = 0;
-int Is_First_Captured = 0;
-
-float frequency = 0;
 
 char getCurrentNote(float frequency) {
 	if(frequency > 50 && frequency <= 91) {
@@ -92,48 +86,6 @@ char getCurrentNote(float frequency) {
 		return 'N';
 	}
 }
-
-void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
-{
-
-	if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
-	{
-		if (Is_First_Captured==0) // if the first rising edge is not captured
-		{
-			IC_Val1 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1); // read the first value
-			Is_First_Captured = 1;  // set the first captured as true
-		}
-
-		else   // If the first rising edge is captured, now we will capture the second edge
-		{
-			IC_Val2 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);  // read second value
-
-			if (IC_Val2 > IC_Val1)
-			{
-				difference = IC_Val2-IC_Val1;
-			}
-
-			else if (IC_Val1 > IC_Val2)
-			{
-				difference = (65535 - IC_Val1) + IC_Val2;
-			}
-
-			float refClock = TIMCLOCK/(PRESCALAR);
-
-			float calculatedFrequency = 0;
-
-			frequency = refClock/difference;
-
-			calculatedFrequency = frequency;
-			measuredFrequency = frequency;
-
-			char currentNote = getCurrentNote(frequency);
-
-			Is_First_Captured = 0; // set it back to false
-		}
-	}
-}
-
 
 
 /* USER CODE END 0 */
